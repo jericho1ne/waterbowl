a115a878f4c5ead26841253b1f62e55ebe49e7e8
Mihai <jericho1ne@yahoo.com>
2015-02-02 01:14:43 -0800

    still need to pass in external dog ID to profile.js

--- app/controllers/profile.js
+++ app/controllers/profile.js
@@ -1,13 +1,30 @@
+//================================================================================
+//		Name:			displayBasicDogInfo( dogInfo, parent_view )
+//		Purpose:	
+//================================================================================
+function displayBasicInfo(poiInfo, parent) {
+	Ti.API.debug("....[~] displayBasicInfo("+dogInfo.place_ID+") called ");
+	
+	var category_icon = ICON_PATH + dogInfo.icon_basic;
+	var rating_df = ICON_PATH + "POI-basic-dogfriendliness.png";
+	var rating_wb = ICON_PATH + "POI-basic-ratingwb.png";
+	parent.add(  myUiFactory.buildInfoBar(category_icon, dogInfo.type, "") );
+	parent.add( myUiFactory.buildSeparator() );
+	parent.add(  myUiFactory.buildInfoBar(rating_df, "Dog friendliness", dogInfo.rating_dogfriendly+"/5") );
+	parent.add( myUiFactory.buildSeparator() );
+	parent.add(  myUiFactory.buildInfoBar(rating_wb, "Rating", dogInfo.rating_dogfriendly+"/5") );
+}
+
+//---------------------------------------------------------------------------------------------------------------
+var args = arguments[0] || {};
 Ti.API.debug( " >>> Dog Array - Profile ::  "+ JSON.stringify( mySesh.dog ) );
-
-
-//  $.user_email.text = mySesh.user.email;
- 
-
-// predefined placeholder values until we hit the backend
-//var mark_title 	 = "Loading mark title...";			// mark.title
-//var mark_text 	 = "Loading mark text...";	// mark.text
-//var mark_subtext = "";							// mark.
 
 var dog_info = mySesh.dog.sex+" / "+mySesh.dog.breed+" / "+mySesh.dog.age+" yrs old";
 $.scrollview.add( myUiFactory.buildPageHeader(mySesh.dog.dog_ID, "profile", mySesh.dog.name, dog_info, "---") );
+
+//-----------------------------------------------------------------------------------------------------------
+//			BASIC INFO
+//-----------------------------------------------------------------------------------------------------------
+var basics_header = myUiFactory.buildSectionHeader("basics_header", "BASIC INFO", 1);
+$.scrollview.add(basics_header);
+//displayBasicDogInfo(dogInfo, $.scrollview);
